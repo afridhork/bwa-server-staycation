ab555fdfce18a83893e6b2cf3ef11e1c4d89e253
Afridho R Kartawiria <afridhorkartawiria@gmail.com>
2023-12-18 16:30:02 +0700

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -14,7 +14,6 @@ const bcrypt = require('bcryptjs')
 module.exports = {
    viewSignin: async (req, res) =>{
       try {
-         console.log('cek');
          const alertMessage = req.flash('alertMessage')
          const alertStatus = req.flash('alertStatus')
          const alert = {message: alertMessage, status: alertStatus}
