8914e2ff4454afec4ad11ab0dfd60ba5b51256b1
Afridho R Kartawiria <afridhorkartawiria@gmail.com>
2023-12-21 21:35:49 +0700

    update vercel.json

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
-const adminController = require('../controllers/adminController')
 var router = express.Router();
 
-router.get('/', adminController.viewSignin);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/admin/signin')
+});
 
 module.exports = router;
